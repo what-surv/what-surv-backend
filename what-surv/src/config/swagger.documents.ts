6ad65e08a22d82c86e4ad0ba86ff0e231488feb1
lee <leehyunk6310@naver.com>
2024-02-05 10:05:43 +0900

    chore : Swagger 추가

---
+++ what-surv/src/config/swagger.documents.ts
@@ -0,0 +1,13 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export class BaseAPIDocument {
+  public builder = new DocumentBuilder();
+  
+  public initializeOptions() {
+    return this.builder
+    	.setTitle('SurveyIT API')
+    	.setDescription('SurveyIT API description')
+    	.setVersion('1.0.0')
+    	.build();
+  }
+}
--- what-surv/src/main.ts
+++ what-surv/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { BaseAPIDocument } from './config/swagger.documents';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 설정
+  const config = new BaseAPIDocument().initializeOptions();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
