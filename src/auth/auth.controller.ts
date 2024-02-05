f4346b9fb7d3a54adfbad38ec2182c8b48666ea0
spartty <65155903+leehyunk6310@users.noreply.github.com>
2024-02-05 22:48:27 +0900

    4 chore google auth setting (#5)
    * feat : 구글 로그인 세팅
    
    * rename : 구글 환경변수 이름 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { Request, Response } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService
+    ) {}
+
+    // 구글 로그인
+    @Get("/login/google")
+    @UseGuards(AuthGuard('google'))
+    async loginGoogle(@Req() req: Request, @Res() res: Response) { }
+
+    // 구글 로그인 redirect
+    @Get("/callback/google")
+    @UseGuards(AuthGuard('google'))
+    async callbackGoogle(@Req() req: Request, @Res() res: Response) {
+        const user = req.user;
+
+        console.log(user);        
+    }
+}
