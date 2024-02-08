bae1b158bac07de7579f597b44403f21c96070eb
spartty <leehyunk6310@naver.com>
2024-02-08 20:22:05 +0900

    feat : DB 연결 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeormConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: TypeormConfig, // TODO: typeorm 설정한 클래스
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env', // 파일의 경로 설정
       isGlobal: true,
