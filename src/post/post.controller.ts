4f27612e8a8284d7970de615cbba999be7a35c90
Daehyun Kim <kimdhyungg@gmail.com>
2024-02-20 22:36:53 +0900

    change all +(plus) to Number() for enhanced readability

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -29,16 +29,16 @@ export class PostController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
+    return this.postService.findOne(Number(id));
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() postUpdateDto: PostUpdateDto) {
-    return this.postService.update(+id, postUpdateDto);
+    return this.postService.update(Number(id), postUpdateDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.postService.remove(+id);
+    return this.postService.remove(Number(id));
   }
 }
