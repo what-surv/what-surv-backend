e153a9aa63e57ccf64ae7c52c8fb088cf6d18db4
Daehyun Kim <kimdhyungg@gmail.com>
2024-02-20 22:11:16 +0900

    Add common entity and apply it to post & user entities

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,4 +1,6 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { CommonEntity } from 'src/common/common.entity';
+import { User } from 'src/user/user.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
 
 export const genderEnum = {
   male: 'Male',
@@ -8,25 +10,34 @@ export const genderEnum = {
 export type Gender = (typeof genderEnum)[keyof typeof genderEnum];
 
 @Entity()
-export class Post {
-  @PrimaryGeneratedColumn()
-  id!: number;
-
+export class Post extends CommonEntity {
+  @Column({ type: 'varchar', length: 255 })
   title!: string;
 
+  @Column({ type: 'timestamp' })
   endDate!: Date;
 
+  @Column({ type: 'enum', enum: genderEnum })
   gender!: Gender;
 
+  @Column('simple-array')
   age!: string[];
 
+  @Column({ type: 'varchar', length: 255 })
   researchType!: string;
 
+  @Column({ type: 'varchar', length: 255 })
   link!: string;
 
+  @Column({ type: 'text' })
   procedure!: string;
 
+  @Column({ type: 'varchar', length: 255 })
   time!: string;
 
+  @Column({ type: 'text' })
   content!: string;
+
+  @ManyToOne(() => User)
+  author!: User;
 }
