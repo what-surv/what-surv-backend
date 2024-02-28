cef46d22f4d95602bcb26b9589807bb1e744392e
kkn1125 <xpfjrnseks1@naver.com>
2024-02-29 03:10:21 +0900

    ✨ feat: add feature to read posts written by user in user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Req,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtUserDto } from 'src/auth/auth.dto';
 import { Public } from 'src/common/utils';
 import { UserService } from './user.service';
 
@@ -10,5 +18,19 @@ export class UserController {
   @Get('nickname-exists')
   async nicknameExists(@Query('nickname') nickname: string) {
     return this.userService.nicknameExists(nickname);
+  }
+
+  /* Added feature to read posts written by specific user */
+
+  /* NOTICE: 공개 API라면 Querystring 반영 + Public 데코 추가 필요 */
+  @Get('me/posts')
+  findAllMyPosts(@Req() req: Request) {
+    const user = req.user as JwtUserDto;
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return this.userService.findAllMyPosts(user);
   }
 }
