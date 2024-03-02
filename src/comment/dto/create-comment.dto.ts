0224021eda6b9aba3608cda939b6d2283fb785ad
kkn1125 <xpfjrnseks1@naver.com>
2024-03-03 04:42:17 +0900

    🛠️ fix: dto와 body, db 관계에 의한 필드명 스네이크케이스로 수정

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -21,5 +21,7 @@ export class CreateCommentDto {
   @ApiProperty()
   @IsInt()
   @IsOptional()
-  parentId?: number;
+  /* dto는 entity의 필드와 같이 데이터베이스의 스네이크 케이스에 맞추는 것이 좋습니다! */
+  /* 이유는 dto를 사용해서 받아오는 body의 키가 save나 insert로 바로 들어가기 떄문에 그렇습니다. */
+  parent_id?: number;
 }
