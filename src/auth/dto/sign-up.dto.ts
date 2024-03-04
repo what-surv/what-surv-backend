b00bd44b15b46ea843f917fa3478767280524d3b
Daehyun Kim <kimdhyungg@gmail.com>
2024-03-05 00:40:46 +0900

    hotfix

--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,33 +1,38 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { Role } from 'src/auth/role/role';
 
 export interface SignUpDto {
   nickname: string;
-  phone: string;
+  phone?: string;
   gender: string;
   advertisingConsent: boolean;
 }
 
 export class AuthSignUpDto implements SignUpDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   public nickname!: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
-  public phone!: string;
+  @IsOptional()
+  public phone?: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   public gender!: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   public advertisingConsent!: boolean;
 
-  @IsString()
-  public job?: string;
-
+  @ApiProperty()
   public role?: Role;
 
+  @ApiProperty()
   public birthDate?: Date;
 }
