bd7b8c1c5af404bb5da81b9f722b004daa2d35f0
Daehyun Kim <kimdhyungg@gmail.com>
2024-03-31 01:16:41 +0900

    feat(app.module): add StaticModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as cookieParser from 'cookie-parser';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { validationPipeProvider } from './common/validation-pipe';
 import { TypeormConfig } from './config/typeorm.config';
@@ -10,6 +12,7 @@ import { RoleExampleModule } from './role-example/role-example.module';
 import { UserModule } from './user/user.module';
 import { CommentModule } from './comment/comment.module';
 import { LikeModule } from './like/like.module';
+import { ImagesModule } from './images/images.module';
 
 const envFilePath =
   process.env.NODE_ENV === 'production' ? '.env' : '.env.development';
@@ -23,12 +26,17 @@ const envFilePath =
       envFilePath,
       isGlobal: true,
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '/public'),
+      serveRoot: '/public',
+    }),
     AuthModule,
     UserModule,
     RoleExampleModule,
     PostModule,
     CommentModule,
     LikeModule,
+    ImagesModule,
   ],
   controllers: [],
   providers: [validationPipeProvider],
