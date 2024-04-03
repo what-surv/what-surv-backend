f028fae78502d8c999b182a291befcb92e6e4686
Daehyun Kim <kimdhyungg@gmail.com>
2024-04-03 11:31:52 +0900

    refactor(ages): remove ages module

--- src/ages/ages.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AgesService } from './ages.service';
-import { AgesController } from './ages.controller';
-
-@Module({
-  controllers: [AgesController],
-  providers: [AgesService],
-})
-export class AgesModule {}
